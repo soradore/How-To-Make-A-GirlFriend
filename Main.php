a0fb4a5287469ca60eb5acc6aa0733e7d9eee4b0
ねらひかだ <hkshirorom@yahoo.co.jp>
2017-08-09 23:04:43 +0900

    first commit

---
+++ Main.php
@@ -0,0 +1,15 @@
+<?php
+
+class GirlFriend{
+	function MakeCoupleEvent(){
+		//by Sora
+	}
+	function DestoryCoupleEvent(){
+		//by Sora
+	}
+	function MakeChildEvent(){
+		//by LUNATiC TiME
+	}
+}
+
+new GirlFriend();

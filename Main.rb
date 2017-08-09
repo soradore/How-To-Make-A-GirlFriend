a5af88d6409308dcfe5a1f1316ece3dfa198ad32
ねらひかだ <hkshirorom@yahoo.co.jp>
2017-08-09 23:16:21 +0900

    add Ruby!!!

---
+++ Main.rb
@@ -0,0 +1,22 @@
+class GirlFriend
+
+	def initialize
+		@init = initialize
+	end
+
+	def MakeCoupleEvent
+		#by Sora
+	end
+
+	def DestoryCoupleEvent
+		#by Sora
+	end
+
+	def MakeChildEvent
+		#by LUNATiC TiME
+	end
+
+end
+
+
+GirlFriend.new

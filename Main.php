4964ea67f65698574fe60b6b3291f4ce302125b6
ねらひかだ <hkshirorom@yahoo.co.jp>
2017-08-09 23:06:43 +0900

    add construct

--- Main.php
+++ Main.php
@@ -1,15 +1,23 @@
 <?php
 
 class GirlFriend{
+
+	function __construct(){
+		echo "Hi, PHPer!";
+	}
+
 	function MakeCoupleEvent(){
 		//by Sora
 	}
+
 	function DestoryCoupleEvent(){
 		//by Sora
 	}
+
 	function MakeChildEvent(){
 		//by LUNATiC TiME
 	}
+
 }
 
 new GirlFriend();
